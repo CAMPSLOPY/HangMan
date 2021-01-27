71fa65826da28b3d711ddae196e3011ed46ae065
CAMPSLOPY <shobowaletola@gmail.com>
2021-01-27 03:21:51 +0100

    app.js

--- .history/app_20210127031615.js
+++ .history/app_20210127031615.js
@@ -4,4 +4,4 @@ const playAgain = document.getElementById("play-button");
 const popUP = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
-const figureParts = document.querySelectorAll(".figure-part");
+const figureParts = document.querySelectorAll("figure");
